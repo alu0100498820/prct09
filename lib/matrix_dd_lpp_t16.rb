bd8bc392906f0d0ec0b15ccd8059a5f31a2c2b33
Borja Barrera Villagrasa <alu0100498820@ull.edu.es>
2013-11-18 15:42:51 +0000

    definiendo las clases y los metodos

--- lib/matrix_dd_lpp_t16.rb
+++ lib/matrix_dd_lpp_t16.rb
@@ -1,5 +1,54 @@
 require "matrix_dd_lpp_t16/version"
 
-module MatrixDdLppT16
-  # Your code goes here...
+#module MatrixDdLppT16
+ 
+class Matriz
+        
+        include Enumerable
+        
+        attr_reader :fil, :col, :matriz
+
+        def initialize(fil, col)
+                @fil = fil 
+		@col = col
+        end
+
 end
+#----------------------------------------------------------------
+
+class Matriz_Densa < Matriz
+
+  def initialize(fil, col)
+  end
+
+  def to_s()
+  end
+
+  def +(other)
+  end
+  
+  def *(other)
+  end
+
+end
+
+####################################################################
+
+class Matriz_Dispersa < Matriz
+
+  def initialize(fil, col)
+  end
+
+  def to_s()
+  end
+
+  def +(other)
+  end
+  
+  def *(other)
+  end
+
+end
+
+#---------------------------------------------------------------- 
+#end
