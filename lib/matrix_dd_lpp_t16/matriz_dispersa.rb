2a2f6d6e46f0a9334fd3d847b4908e47e65923d6
Borja Barrera Villagrasa <alu0100498820@ull.edu.es>
2013-11-20 17:53:10 +0000

    resolviendo espectativa matriz dispersa

--- lib/matrix_dd_lpp_t16/matriz_dispersa.rb
+++ lib/matrix_dd_lpp_t16/matriz_dispersa.rb
@@ -17,6 +17,42 @@ module MatrixDdLppT16
       new_initialize
     end
 
+  # Creacion matriz dispersa
+    def self.copy(matriz)
+
+    obj = new(matriz.filas, matriz.columnas)
+
+    for i in 0...matriz.filas
+      for j in 0...matriz.columnas
+        val = matriz.get(i,j)
+        if(val != 0)
+          obj.pos[i][j] = val
+          end
+      end
+    end
+    obj
+    end
+
+  # Get
+
+   def get(i, j)
+      if(@pos[i][j] != nil)
+        return @pos[i][j]
+      else
+        return 0
+      end
+    end
+ 
+  # Set
+
+    def set(i, j, val)
+      if(val == nil or val == 0)
+        @pos[i].delete(j)
+      else
+        @pos[i][j] = val
+      end
+    end
+
   end
 
 end
