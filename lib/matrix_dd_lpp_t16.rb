58b12cb2b4e4498ddd8fa629bffb42b82490b676
Borja Barrera Villagrasa <alu0100498820@ull.edu.es>
2013-11-20 12:39:30 +0000

    añadiendo require

--- lib/matrix_dd_lpp_t16.rb
+++ lib/matrix_dd_lpp_t16.rb
@@ -1,6 +1,7 @@
 require "./lib/matrix_dd_lpp_t16/version"
 require "./lib/matrix_dd_lpp_t16/matriz.rb"
 require "./lib/matrix_dd_lpp_t16/fraccion.rb"
+require "./lib/matrix_dd_lpp_t16/matriz_densa.rb"
 
 
 module MatrixDdLppT16
