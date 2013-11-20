62cb8a3b1cf8c1da397957d0677d2078fffd7d5c
Borja Barrera Villagrasa <alu0100498820@ull.edu.es>
2013-11-20 12:42:59 +0000

    subiendo ficheros matriz_densa y matriz_dispersa

---
+++ lib/matrix_dd_lpp_t16/matriz_densa.rb
@@ -0,0 +1,63 @@
+require "./lib/matrix_dd_lpp_t16/matriz.rb"
+
+module MatrixDdLppT16
+
+  class Matriz_Densa < Matriz
+
+    
+    def initialize(filas, columnas)
+      super
+      @pos = Array.new(@filas,0)
+      for i in 0...@filas
+        @pos[i] = Array.new(@columnas,0)
+      end
+    end
+
+
+ # Get (Devuelve el valor guardado del elemento en i,j)
+    def get(i, j)
+      @pos[i][j]
+    end
+
+ # Set (Modifica el valor guardado del elemento en i,j)
+    def set(i, j, val)
+      @pos[i][j] = val
+    end
+
+ 
+#Metodo to_s  
+    def to_s
+      s = ""
+      for i in 0...@filas
+        for j in 0...@columnas
+          s += "#{@pos[i][j].to_s} "
+        end
+        puts " "
+      end
+      s
+    end
+
+
+ # Suma
+    def +(other)
+      c = Matriz_Densa.new(@filas, @columnas)
+      for i in 0...@filas
+        for j in 0...@columnas
+          c.set(i, j, get(i,j) + other.get(i,j))
+        end
+      end
+      c
+    end
+
+# Resta        
+    def -(other)
+      c = Matriz_Densa.new(@filas, @columnas)
+      for i in 0...@filas
+        for j in 0...@columnas
+          c.set(i, j, get(i,j) - other.get(i,j))
+        end
+      end
+      c
+    end
+end
+end
---
+++ lib/matrix_dd_lpp_t16/matriz_dispersa.rb
@@ -0,0 +1,10 @@
+require "./lib/matrix_dd_lpp_t16/matriz.rb"
+require "./lib/matrix_dd_lpp_t16/matriz_densa.rb"
+
+module MatrixDdLppT16
+
+  class Matriz_Dispersa < Matriz
+
+  end
+
+end
