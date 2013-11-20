bb601eab41fe9d28dddba72c54e4a8a52255ea63
Borja Barrera Villagrasa <alu0100498820@ull.edu.es>
2013-11-20 13:06:20 +0000

    definiendo initialize en matriz dispersa

--- lib/matrix_dd_lpp_t16/matriz_dispersa.rb
+++ lib/matrix_dd_lpp_t16/matriz_dispersa.rb
@@ -5,6 +5,18 @@ module MatrixDdLppT16
 
   class Matriz_Dispersa < Matriz
 
+    def new_initialize
+      @pos = Array.new(@filas)
+      for i in 0...@filas
+        @pos[i] = {} 
+      end
+    end
+
+    def initialize(filas, columnas)
+      super
+      new_initialize
+    end
+
   end
 
 end
