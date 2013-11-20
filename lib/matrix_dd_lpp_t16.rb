9e86e7f8102b1f548fa8b9c9952902f04ec4f428
Borja Barrera Villagrasa <alu0100498820@ull.edu.es>
2013-11-20 12:44:36 +0000

    modificando fichero matriz_dd_lpp_t16

--- lib/matrix_dd_lpp_t16.rb
+++ lib/matrix_dd_lpp_t16.rb
@@ -2,6 +2,7 @@ require "./lib/matrix_dd_lpp_t16/version"
 require "./lib/matrix_dd_lpp_t16/matriz.rb"
 require "./lib/matrix_dd_lpp_t16/fraccion.rb"
 require "./lib/matrix_dd_lpp_t16/matriz_densa.rb"
+require "./lib/matrix_dd_lpp_t16/matriz_dispersa.rb"
 
 
 module MatrixDdLppT16
