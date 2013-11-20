5396f0f12de9a4c4140872ee60f95d6f36d3f0fd
Ayose Castillo Barroso <alu0100600810@ull.edu.es>
2013-11-20 11:50:45 +0000

    subiendo fichero fraccion.rb

---
+++ lib/matrix_dd_lpp_t16/fraccion.rb
@@ -0,0 +1,28 @@
+
+module MatrixDdLppT16
+	class Fraccion
+
+	  # Módulo Comparable
+	  include Comparable 
+	  
+	  attr_reader :num, :dem
+	  def initialize(num,dem)
+		@num, @dem = num, dem
+		mcm = gcd(num, dem)
+		    @num = num/mcm
+		    @dem = dem/mcm
+	  end
+
+	  def gcd(u, v)
+	     u, v = u.abs, v.abs
+	     while v != 0
+	       u, v = v, u % v
+	     end
+	    u
+	  end
+
+	  def to_s
+	    "#{@num}/#{@dem}"
+	  end
+	end
+end
